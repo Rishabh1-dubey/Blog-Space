d07e8fd76b6226eb998f1001479f6b9d03629387
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-11-05 14:09:09 +0530

    adding routes for pages

---
+++ frontend/src/Pages/Signup.tsx
@@ -0,0 +1,23 @@
+import { Auth } from "../components/Auth";
+import { Quote } from "../components/Quote";
+
+export const Signup = () => {
+  return (
+    <div>
+      <div className=" grid  grid-cols-1 md:grid-cols-2">
+
+        {/* -------------------*/}
+        <div>
+          <Auth />
+        </div>
+        {/* -------------- */}
+
+        {/* ------------------ */}
+        <div className="hidden md:block">
+          <Quote />
+        </div>
+        {/* ---------------------- */}
+      </div>
+    </div>
+  );
+};
