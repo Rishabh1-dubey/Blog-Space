38327ac6ba67a418f1d95a0426dbcab97bc2f3bc
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-11-06 23:55:41 +0530

    adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import { Hono } from "hono";
 import { blogRouter } from "./routes/Blog";
 import { userRouter } from "./routes/User";
-
-
+import { cors } from "hono/cors";
 
 
 //{
@@ -14,14 +13,13 @@ const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
-  },
-  Variables:{
-    userId: string
-  }
+  };
+  Variables: {
+    userId: string;
+  };
 }>();
-
+app.use('/*', cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
-
 
 export default app;
