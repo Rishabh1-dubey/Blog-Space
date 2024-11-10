62bd6aa7f7195fbfb87eaea8472ffed714fd1703
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-11-10 23:11:35 +0530

    adding single blog custom hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
-interface Blog {
-  content: string;
-  title: string;
+export interface Blog {
   id: string;
+  title: string;
+  content: string;
   author: {
     name: string;
   };
 }
+//this id for bulk blog
 export const useBlogs = () => {
   const [loading, setLoading] = useState(true);
-  const [blogs, setBlog] = useState<Blog[]>([]);
+  const [blogs, setBlogs] = useState<Blog[]>([]);
 
   useEffect(() => {
     axios
@@ -24,7 +25,7 @@ export const useBlogs = () => {
         },
       })
       .then((response) => {
-        setBlog(response.data.blogs);
+        setBlogs(response.data.blogs);
         // console.log(response.data);
         setLoading(false);
       })
@@ -32,11 +33,43 @@ export const useBlogs = () => {
         console.log("Error fetching blogs:", error);
         setLoading(false);
       });
-      // console.log(localStorage.getItem("token"))
+    // console.log(localStorage.getItem("token"))
   }, []);
 
   return {
     blogs,
+    loading,
+  };
+};
+
+//this for single blog
+export const useBlog = ({ id }: { id: string }) => {
+  const [loading, setLoading] = useState(false);
+  const [blog, setBlog] = useState<Blog>();
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+          //here you defind Bearer in you auth app so you have to add bearer before sending the data into local strogae it is compulsory
+        },
+      })
+      .then((response) => {
+        setBlog(response.data.blog);
+        // console.log(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error fetching blogs:", error);
+        setLoading(false);
+      });
+    // console.log(localStorage.getItem("token"))
+  }, [id]);
+
+  return {
+    blog,
     loading,
   };
 };
